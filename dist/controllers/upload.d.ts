c4bfb69856ba1936be5c401533677cc6e65d7aa5
Anderson <andersondasilvabarretoasb@gmail.com>
2026-02-12 21:46:49 -0300

    Removendo arquivos ignorados

--- dist/controllers/upload.d.ts
+++
@@ -1,3 +0,0 @@
-import type { RequestHandler } from "express";
-export declare const upload: RequestHandler;
-//# sourceMappingURL=upload.d.ts.map
--- dist/controllers/upload.js
+++
@@ -1,4 +0,0 @@
-export const upload = (req, res) => {
-    res.json({});
-};
-//# sourceMappingURL=upload.js.map
